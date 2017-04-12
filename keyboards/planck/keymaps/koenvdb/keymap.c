c37888fe0016bc2811c07c528a2462f053f97069
Koenvdb <k.vanderborght@student.fontys.nl>
2017-04-12 08:58:19 +0200

    Update program layer toggle

--- keyboards/planck/keymaps/koenvdb/keymap.c
+++ keyboards/planck/keymaps/koenvdb/keymap.c
@@ -25,6 +25,7 @@ enum planck_layers {
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
+  PROGM,
   LOWER,
   RAISE,
   BACKLIT
@@ -97,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {MO(_PROGM),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {PROGM,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
   {KC_LCTL, KC_LALT, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   NEWD,    CLOSED,  BACKD,   NEXTD}
 },
@@ -263,9 +264,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case PROGM:
       if (record->event.pressed) {
+        layer_on(_PROGM);
         backlight_toggle();
       }
       else {
+        layer_off(_PROGM);
         backlight_toggle();
       }
       return false;
