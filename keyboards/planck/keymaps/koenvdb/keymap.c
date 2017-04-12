33b61e026440bb170c2f1bdb1ef501f49488cb42
Koenvdb <k.vanderborght@student.fontys.nl>
2017-04-12 10:06:47 +0200

    maybe fixed backlighting

--- keyboards/planck/keymaps/koenvdb/keymap.c
+++ keyboards/planck/keymaps/koenvdb/keymap.c
@@ -267,11 +267,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case PROGM:
       if (record->event.pressed) {
         layer_on(_PROGM);
-        backlight_level(3);
+        backlight_toggle();
       }
       else {
         layer_off(_PROGM);
-        backlight_level(0);
+        backlight_toggle();
       }
       return false;
       break;
